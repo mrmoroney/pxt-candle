dafa86c9bbd4fe332080b825e105fee79a5cbdd0
mrmoroney <56696526+mrmoroney@users.noreply.github.com>
2026-05-06 16:16:22 +0100

    Delete test.ts

--- test.ts
+++
@@ -1,9 +0,0 @@
-//% color="#0000ff" icon="\uf06d" block="Test"
-namespace test {
-
-    //% block="set test value %val"
-    //% val.shadow="range" val.min=0 val.max=100 val.defl=50
-    export function setValue(val: number): void {
-        basic.showNumber(val)
-    }
-}
