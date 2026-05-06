03fe3b48e8b3f9c2fc4f36b61cef2c50216be595
mrmoroney <56696526+mrmoroney@users.noreply.github.com>
2026-05-06 15:44:13 +0100

    Update block definition for setValue function

--- test.ts
+++ test.ts
@@ -1,7 +1,8 @@
 //% color="#0000ff" icon="\uf06d" block="Test"
 namespace test {
 
-    //% block
+    //% block="set test value %val"
+    //% val.shadow="range" val.min=0 val.max=100 val.defl=50
     export function setValue(val: number): void {
         basic.showNumber(val)
     }
