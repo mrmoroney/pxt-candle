953337f506c514bec8e97b3816b9c7ee0264290e
mrmoroney <56696526+mrmoroney@users.noreply.github.com>
2026-05-06 15:29:50 +0100

    Refactor setValue function block definition
    Removed comments and adjusted block definition for setValue function.

--- test.ts
+++ test.ts
@@ -1,11 +1,7 @@
 //% color="#0000ff" icon="\uf06d" block="Test"
 namespace test {
 
-    /**
-     * Test slider
-     */
-    //% block="set value %val"
-    //% val.shadow="range" val.min=0 val.max=100 val.defl=50
+    //% block
     export function setValue(val: number): void {
         basic.showNumber(val)
     }
